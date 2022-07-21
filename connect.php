ffe7c325199c5e430f7a51f53ed86a2f13c5f2a9
oderathegreat <oderathegreat@gmail.com>
2022-07-21 12:20:51 +0300

    process for order reservation

---
+++ connect.php
@@ -0,0 +1,44 @@
+<?php
+//establish a connection to our server
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password);
+
+// // Check connection
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
+?>
---
+++ process.php
@@ -0,0 +1,74 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "caffedb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if(isset($_POST['submit'])) {
+
+    if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) ) {
+
+        $fullname = $_POST['name'];
+        $email = $_POST['email'];
+        $phonenumber = $_POST['phone'];
+
+        //var_dump($fullname,$email,$phonenumber);
+
+        //Insert Data to database 
+        $query = "insert into customers(fullnames,email,phonenumber) values('$fullname' , '$email', '$phonenumber')";
+     
+       //run query
+       $run = mysqli_query($conn, $query) or die('Error: ' . mysqli_error($conn));;
+
+       //check if our query runs
+       if ($run) {
+        echo 'Reservation made successfully';
+       }
+       else {
+        echo 'Data not  submitted';
+       }
+
+
+    }
+
+    else {
+        echo 'All fields are required';
+       
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
